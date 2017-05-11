b53f5cf392743d4969510904ec8e649e90f0f721
Simon Chen <simon.c312@gmail.com>
2017-05-11 16:35:04 -0700

    display a static remote widget

---
+++ app/src/main/java/is/hellos/demos/widget/RespirationWidgetProvider.java
@@ -0,0 +1,62 @@
+package is.hellos.demos.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import is.hellos.demos.R;
+import is.hellos.demos.activities.RespirationActivity;
+
+/**
+ * Created by simonchen on 5/11/17.
+ */
+
+public class RespirationWidgetProvider extends AppWidgetProvider {
+    /**
+     * Not called if configuration activity defined
+     * @param context
+     * @param appWidgetManager
+     * @param appWidgetIds
+     */
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        final int N = appWidgetIds.length;
+
+        //TODO start service to update remote views because need to fetch data.
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, RespirationActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.respiration_appwidget);
+            views.setOnClickPendingIntent(R.id.respiration_appwidget_view, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+}
